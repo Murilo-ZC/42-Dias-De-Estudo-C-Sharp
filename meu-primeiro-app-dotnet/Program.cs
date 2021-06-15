d096de0d15affd15a17c480ed2a854e74314a1b1
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-06-15 07:41:43 -0300

    Criação do primeiro app dotnet

---
+++ meu-primeiro-app-dotnet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace meu_primeiro_app_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("#42DiasDeEstudo");
+            Console.WriteLine("Data e Hora Atual:" + DateTime.Now);
+        }
+    }
+}
